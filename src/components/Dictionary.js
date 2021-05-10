addce1caa58820c83617fdb99dc5cb7d7c549e30
PiaLember <pia.lember@gmail.com>
2021-05-10 12:35:36 -0500

    Improve language selection

--- src/components/Dictionary.js
+++ src/components/Dictionary.js
@@ -7,16 +7,24 @@ function Dictionary() {
   let [results, setResults] = useState(null);
   let [clicked, setClicked] = useState("🇺🇸 What word do you want to look up?");
   let [language, setLanguage] = useState("en_US");
+  let [buttonText, setButtonText] = useState("🇩🇪 DE");
 
-  function handleClick() {
+  function showDE() {
     setClicked("🇩🇪 Welches Wort möchten Sie nachschlagen?");
     setLanguage("de");
+    setButtonText("🇺🇸 EN");
+    setResults(null);
   }
-
+  function showEN() {
+    setClicked("🇺🇸 What word do you want to look up?");
+    setLanguage("en_US");
+    setButtonText("🇩🇪 DE");
+    setResults(null);
+  }
   function hanleResponse(response) {
     setResults(response.data[0]);
+    setInputText("");
   }
-
   function search(event) {
     // documentation: https://dictionaryapi.dev
     let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/${language}/${inputText}`;
@@ -24,7 +32,6 @@ function Dictionary() {
 
     event.preventDefault();
   }
-
   function handleInput(event) {
     let newText = event.target.value;
     setInputText(newText);
@@ -32,15 +39,33 @@ function Dictionary() {
 
   return (
     <div className="Dictionary">
-      <button onClick={handleClick}>DE</button>
-      <h4>{clicked}</h4>
-      <form onSubmit={search}>
-        <input
-          type="search"
-          onChange={handleInput}
-          placeholder="_ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _✍︎"
-        />
-      </form>
+      {language === "en_US" ? (
+        <div>
+          <button onClick={showDE}>{buttonText}</button>
+          <h4>{clicked}</h4>
+          <form onSubmit={search}>
+            <input
+              type="search"
+              onChange={handleInput}
+              placeholder="_ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _✍︎"
+              value={inputText}
+            />
+          </form>
+        </div>
+      ) : (
+        <div>
+          <button onClick={showEN}>{buttonText}</button>
+          <h4>{clicked}</h4>
+          <form onSubmit={search}>
+            <input
+              type="search"
+              onChange={handleInput}
+              placeholder="_ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _✍︎"
+              value={inputText}
+            />
+          </form>
+        </div>
+      )}
 
       <Result results={results} />
     </div>
