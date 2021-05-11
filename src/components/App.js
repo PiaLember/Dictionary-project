316fe358a0261078f807c02f369ceec19c03640b
PiaLember <pia.lember@gmail.com>
2021-05-11 10:13:19 -0500

    Add style

--- src/components/App.js
+++ src/components/App.js
@@ -4,7 +4,7 @@ function App() {
   return (
     <div className="App">
       <div className="container">
-        <small>Coded by PiaL</small>
+        <small>｛Coded by: PiaLe｝</small>
         <header className="App-header">
           <h1>Dictionary</h1>
         </header>
